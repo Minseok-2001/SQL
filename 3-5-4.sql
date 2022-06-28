62363b7f77253bab47531cb8909b48a8ae66cb65
200000001 <90817465+200000001@users.noreply.github.com>
2022-06-28 22:09:48 +0900

    Update 3-5-4.sql

--- 3-5-4.sql
+++ 3-5-4.sql
@@ -2,4 +2,4 @@ select stamp
 ,url
 ,split(regexp_extract(url, '//[^/]+([^?#]+)'), '/')[safe_ordinal(2)] as path 1
 ,split(regexp_extract(url, '//[^/]+([^?#]+)'), '/')[safe_ordinal(3)] as path 2
-from access_log
+from access_log;
