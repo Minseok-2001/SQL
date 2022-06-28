83d19311dc0fb115c3b6282058543210db22966f
200000001 <90817465+200000001@users.noreply.github.com>
2022-06-28 22:16:22 +0900

    Create 3-5-7.sql

---
+++ 3-5-7.sql
@@ -0,0 +1,6 @@
+select stamp
+,extract(year from stamp) as year
+,extract(month from stamp) as month
+,extract(day from stamp) as day
+,extract(hour from stamp) as hour
+from (select cast('2016-01-30 12:00:00' as timestamp) as stamp) as t
